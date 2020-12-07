0823e37548f4f03202200c9b9b317a7a12b9d599
Ally Thach <allythach379@gmail.com>
2020-12-06 17:21:45 -0800

    test light

--- source/main.c
+++ source/main.c
@@ -12,12 +12,29 @@
 #include "simAVRHeader.h"
 #endif
 
+void transmit_data(unsigned char data){
+    int i;
+    for(i = 0; i<8; ++i){
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTC = 0x08;
+        // set SER = next bit of data to be sent.
+        PORTC |= ((data >> i) & 0x01);
+        // set SRCLK = 1 Rising edge shifts next bit of data into the shift register
+        PORTC |= 0x02;
+    }
+    //set RCLK = 1. Rising edge copies data from "Shift" register to "Storage" register
+    PORTC |= 0x04;
+    //clears all lines in preparation of a new transmission
+    PORTC = 0x00;
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRC = 0xFF;
+    PORTC = 0x00;
     /* Insert your solution below */
     while (1) {
-
+        PORTC[5] = 1;
     }
     return 1;
 }
