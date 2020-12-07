055b38bb03dd94efed35b77e4fa2b1ed171089a3
Ally Thach <allythach379@gmail.com>
2020-12-06 17:37:18 -0800

    try portb

--- source/main.c
+++ source/main.c
@@ -17,27 +17,27 @@ void transmit_data(unsigned char data){
     for(i = 0; i<8; ++i){
         // Sets SRCLR to 1 allowing data to be set
         // Also clears SRCLK in preparation of sending data
-        PORTC = 0x08;
+        PORTB = 0x08;
         // set SER = next bit of data to be sent.
-        PORTC |= ((data >> i) & 0x01);
+        PORTB |= ((data >> i) & 0x01);
         // set SRCLK = 1 Rising edge shifts next bit of data into the shift register
-        PORTC |= 0x02;
+        PORTB |= 0x02;
     }
     //set RCLK = 1. Rising edge copies data from "Shift" register to "Storage" register
-    PORTC |= 0x04;
+    PORTB |= 0x04;
     //clears all lines in preparation of a new transmission
-    PORTC = 0x00;
+    PORTB = 0x00;
 }
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-    DDRC = 0xFF;
-    PORTC = 0x00;
+    DDRB = 0xFF;
+    PORTB = 0x00;
     DDRD = 0xFF;
     PORTD = 0x00;
     /* Insert your solution below */
     while (1) {
-        PORTC = 0xFF;
+        PORTB = 0xFF;
         PORTD = 0x00;
     }
     return 1;
